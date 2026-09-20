c4d9eaff70ca76e8b5ca30639c2a6957b5aef114
milanstevic1992ts-gif <milanstevic1992ts@gmail.com>
2026-09-20 11:46:03 +0200

    Expose GE360 geometry engine API

---
+++ geometry-engine/index.js
@@ -0,0 +1,4 @@
+export { solveFloorPlan } from "./solver.js";
+export { validateFloorPlan } from "./validation.js";
+export { MODE_PRESETS, DEFAULT_OPTIONS, ENGINE_VERSION } from "./constants.js";
+export { openingIntervalFromA } from "./openings.js";
